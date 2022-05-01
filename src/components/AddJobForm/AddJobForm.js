360868b8658a072baecc473c004fd098cf3b6001
Nicole <nlimtung@gmail.com>
2022-05-01 15:18:05 -0400

    homepage css

--- src/components/AddJobForm/AddJobForm.js
+++ src/components/AddJobForm/AddJobForm.js
@@ -1,38 +1,32 @@
 import React from "react";
+import "./AddJobForm.css"
 
 export default function AddJobForm(props) {
     return(
-        <div>
+        <div className="AddJobForm">
             <h2>Add more job:</h2>
             <form onSubmit={(e) =>props.handleSubmit(e)}>
-                <label> Company name</label>
+
+                <label> Company name:</label>
                 <input
                 name = "company"
                 value = {props.company}
                 onChange= {props.handleChange}
-                /><br></br>
+                /><br></br><br></br>
 
-                {/* <label>Date Applied</label>
-                <input
-                name = "dateApplied"
-                type =  "date"
-                value = {props.dateApplied}
-                onChange= {(e)=>props.handleChange(e)}
 
-                /> */}
-
-                <label>Link</label>
+                <label>Link: </label>
                 <input
                     name = "postLink"
                     value = {props.postLink}
                     onChange= {(e)=>props.handleChange(e)}
-                />
+                    /><br></br><br></br>
 
 
                 <button
-                
-                type = "submit"
-                >Submit</button>
+                    type = "submit"
+                    >Submit
+                </button>
             </form>
         </div>
     )
